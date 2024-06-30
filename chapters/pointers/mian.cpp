22beb197f2a2ec979afca44c2e51e9fd1d91a5b0
Kanishka Sahu <34833039+sahukanishka@users.noreply.github.com>
2024-06-30 20:58:12 +0530

    pointer added in chater

--- chapters/pointers/mian.cpp
+++ chapters/pointers/mian.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
-
+using namespace std;
 int main(){
     int x = 10;
-    int *x_pointer ;
+    int *x_pointer = &x;
     int **y_pointer = &x_pointer ;
-    std::cout<<"Output : "<<x<<std::endl;
-    std::cout<<"Output : "<<*x_pointer<<std::endl;
-    x_pointer = &x;
-    std::cout<<"Output : "<<x_pointer<<std::endl;
-    std::cout<<"Output : "<<*x_pointer<<std::endl;
+    std::cout<<"Memory address of x "<<x_pointer<<std::endl;
+    std::cout<<"Value of x from pointer "<<*x_pointer<<std::endl;
+    std::cout<<"Memory address of x_pointer : "<<y_pointer<<std::endl;
+    std::cout<<"Value of x_pointer : "<<*y_pointer<<std::endl;
     *x_pointer = *x_pointer + 1;
     std::cout<<"Output : "<<x_pointer<<std::endl;
     std::cout<<"Output : "<<*x_pointer<<std::endl;
     std::cout<<"Output : "<<*y_pointer<<std::endl;
-     std::cout<<"Output : "<<**y_pointer<<std::endl;
+    std::cout<<"Output : "<<**y_pointer<<std::endl;
     **y_pointer = 12;
-    
     std::cout<<"Output : "<<x_pointer<<std::endl;
     std::cout<<"Output : "<<*x_pointer<<std::endl;
     std::cout<<"Output : "<<*y_pointer<<std::endl;
     std::cout<<"Output : "<<**y_pointer<<std::endl;
-    
 
-    
+    int var[3] = {10,20,30};
+    int *ptr;
+    ptr = var;
+    int length = sizeof(var) / sizeof(var[0]);
+    for(int a = 0;a < length;a++){
+        cout<<"--> "<<a<<" "<<ptr<<endl;
+        cout<<"--> "<<a<<" "<<*ptr<<endl;
+        ptr++;
+    }
+
+    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int (*ptr_matrix)[3] = matrix;
+
+    1,2,3 - 0,0 - 0,1 - 0,2 
+    4,5,6 - 1,0 - 1,1 - 1,2
+    7,8,9 - 2,0 - 2,1 - 2,2
+
+    for(int i = 0; i < 3 ; i++){
+       for(int j = 0;j < 3;j++){
+         cout<<i<<","<<j<<" --> "<<matrix[i][j]<<endl;
+         cout<<*(*(ptr_matrix+i)+j) <<endl;
+       }
+    }
+
+
    
 }
--- chapters/pointers/readme.md
+++ chapters/pointers/readme.md
@@ -1,1 +1,33 @@
 # Pointers
+Pointers are nothing but a way to store a memory address of a value. You can store any data type memory address and by pointer you can manipulate the value that is stored in the same memory address.
+
+To declare pointer we use ```*``` sign.
+ex. datatype *var_name; 
+```cpp
+int x = 10;
+int *x_pointer = &x;
+```
+Here x is integer and x_pointer is a pointer and it stores the memory address of int x (&x gives us memory address at which the value of x is stored).
+
+```cpp
+ std::cout<<"Memory address of x "<<x_pointer<<std::endl;
+ std::cout<<"Value of x from pointer "<<*x_pointer<<std::endl;
+```
+
+We can create another pointer that can stored the memory address of pointer that points to variable. In the above example we have x and x_pointer that points to memory address of x now create a new pointer called y_pointer that points to x_pointer. To create this we need to use 2 ```*``` sign.
+
+```cpp
+int **y =_pointer = &x_pointer;
+std::cout<<"Memory address of x_pointer : "<<y_pointer<<std::endl;
+std::cout<<"Value of x_pointer : "<<*y_pointer<<std::endl;
+```
+### Output - 
+
+```
+Memory address of x 0x16bc2ae1c
+Value of x from pointer 10
+Memory address of x_pointer : 0x16bc2ae10
+Value of x_pointer : 0x16bc2ae1c
+```
+Now you can see the output that x_pointer stores the same memory address as value of y_pointer do. 
+
