2b3949e32a0bfc2c0f53174ff25259b3af63c66b
Kanishka Sahu <34833039+sahukanishka@users.noreply.github.com>
2024-07-04 22:12:07 +0530

    classes added

--- chapters/classes/main.cpp
+++ chapters/classes/main.cpp
@@ -1,25 +1,67 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+int main()
+{
+    class Car
+    {
+    public:
+        int price;
+        int topSpeed;
+        string brand;
 
-int main(){
-    class Car {
-        public:
-            int price;
-            int topSpeed;
-            char brand;
-
-            int calculateOnRoadPrice(int tax){
-                int finalCost = tax + price;
-                return finalCost;
-            }
+        int calculateOnRoadPrice(int tax)
+        {
+            int finalCost = tax + price;
+            return finalCost;
+        }
     };
 
-    Car bmw ;
-    bmw.brand = 'bmw';
+    Car bmw;
+    bmw.brand = "bmw";
     bmw.price = 99999;
     bmw.topSpeed = 280;
 
-    cout<<"Car"<<endl;
-   
+    cout << "Car" << endl;
+    cout << "Price : " << bmw.price << endl;
+    cout << "Brand : " << bmw.brand << endl;
+
+    int onRoadPrice = bmw.calculateOnRoadPrice(100);
+    cout << "On Road Price : " << onRoadPrice << endl;
+
+    struct Truck
+    {
+        int price;
+        string model;
+        string getPrivateData(const string &value)
+        {
+            if (value == "engine")
+            {
+                return engine;
+            }
+            return "";
+        }
+
+        void setPrivateData(const string &value, const string &data)
+        {
+            if (value == "engine")
+            {
+                engine = data;
+            }
+        }
+
+    private:
+        string engine;
+    };
+
+    Truck tata;
+    tata.model = "16wheel";
+    tata.price = 1999999;
+    tata.setPrivateData("engine", "12hrs");
+
+    cout << "Price : " << tata.price << endl;
+    cout << "Brand : " << tata.model << endl;
+    cout << "Engine : " << tata.getPrivateData("engine") << endl;
 }
--- chapters/classes/readme.md
+++ chapters/classes/readme.md
@@ -1,1 +1,35 @@
 # Classes
+Classes are the building block or object [oriented programming](). Lets understand the classes in simpler way. Suppose you want to create a program that can tell the specs of a car. Lets create a bunch of variables to define our car.
+
+```c
+string carModel = "s-class";
+string carEngine = "1200cc",
+int topSpeed = 200;
+int price = 999999;
+```
+
+As you can see we have car model,engine,price and top speed of car. Suppose we want to create second car with different specs the we need to copy paste all the variables with with new values. Think if we have 100's of these variable and if we want to add new property in car we have to make changes in 100s of car. Here classes will help us.
+
+**Classes**  are nothing but user defined data type with holds its own data member and data functions. These data can be accessed and used by creating a instance of car class.
+
+```c
+int main()
+{
+    class Car
+    {
+    public:
+        //data members 
+        int price;
+        int topSpeed;
+        string brand;
+        //data function
+        int calculateOnRoadPrice(int tax)
+        {
+            int finalCost = tax + price;
+            return finalCost;
+        }
+    };
+}
+```
+
+By default class created a private data members and function, means you can access these directly. So if you want to use data members and function to use directly you have to use keyword **public** before initializing variables. 
